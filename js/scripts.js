2eadba262b8f5c79db2595f9ea2bf7fc43f5f0ca
Augustine von Trapp and Stephanie Gurung <augustine@>
2016-06-07 13:59:40 -0700

    add a recursive function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function factorial(num) {
+
+  if (num <= 0) {
+    return 1;
+  }else {
+    return (num * factorial(num - 1));
+  }
+}
+
+
+
+
+
+var result = factorial(5);
+
+console.log(result)
